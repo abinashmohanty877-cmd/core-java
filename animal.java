2ca3e295357c9e6799145491566e7a2de412128c
ABINASH <abinashmohanty877@gmail.com>
2026-09-08 15:39:31 +0530

    Add abstract class structure with implementation example

---
+++ animal.java
@@ -0,0 +1,18 @@
+ public class animal {
+	animal()
+	{
+		System.out.println("tiger");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		{
+			animal t1= new animal();
+		}
+
+	}
+
+}
+
+    
+
