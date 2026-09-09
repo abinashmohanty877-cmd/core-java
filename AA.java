15b93c2d6c4a4c5a96d78208704584d9ded0c3b0
ABINASH <abinashmohanty877@gmail.com>
2026-09-09 22:37:22 +0530

    class added

---
+++ AA.java
@@ -0,0 +1,16 @@
+class M{
+    public void m1(int roll){
+     System.out.println(roll);
+
+}
+public void b1(String colour){
+  System.out.println(colour);
+}
+}
+public class    AA{
+    public static void main(String[] args) {
+        M m = new M();
+       m.m1(1200000);
+       m.b1("green");
+    }
+}
