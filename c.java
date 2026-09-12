c3214b5c39073dd2efebc9bacdd9e5183fbeee32
ABINASH <abinashmohanty877@gmail.com>
2026-09-12 19:50:07 +0530

    c.java

---
+++ c.java
@@ -0,0 +1,22 @@
+public class c {
+    public Object m1()
+    {
+        System.out.println("tommyyy");
+        return null;
+    }
+}
+class B extends c
+{
+    public String m1()
+    {
+        System.out.println("dogs");
+        return null;
+    }
+}
+class Test
+{
+    public static void main(String[] args) {
+        B b = new B();
+        b.m1();
+    }
+}
