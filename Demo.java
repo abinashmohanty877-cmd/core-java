7a7da88f6b650be9a69e7904a40c2213c95f95c1
ABINASH <abinashmohanty877@gmail.com>
2026-09-17 14:11:12 +0530

    Demo.java

---
+++ Demo.java
@@ -0,0 +1,10 @@
+class Demo{
+    public void m1()
+    {
+        System.out.println("thorrrr");
+    }
+    public static void main(String[] args){
+        Demo d1 = new Demo();
+    d1.m1();   
+}
+}
