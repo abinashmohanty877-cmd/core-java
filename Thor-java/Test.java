a5186b07706a00e9d103397cfd85c1ebbbe9d430
ABINASH <abinashmohanty877@gmail.com>
2026-09-20 15:08:51 +0530

    Test.java

---
+++ Thor-java/Test.java
@@ -0,0 +1,13 @@
+class Test {
+    public void m1(int f, int s)
+    {
+        System.out.println(f+s);
+    }
+    public static void main(String[] args)
+    {
+        Test n1 = new Test();
+        n1.m1(545,4546847);
+    }
+
+    
+}
