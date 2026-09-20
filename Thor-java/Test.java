40fec380a133646d2950fa3a551ddec1501c9f26
ABINASH <abinashmohanty877@gmail.com>
2026-09-20 17:41:23 +0530

    Test.java

--- Thor-java/Test.java
+++ Thor-java/Test.java
@@ -1,24 +1,24 @@
 class A{
-    public void m1()
+    public A m1()
     {
-        System.out.println("ktm");
+        System.out.println("lionnnn");
+        return new A();
     }
 }
-    class B extends A{
-        public void m1()
-        {
-            System.out.println("bullet");
-        }
-    
-
+class B extends A{
+    public  B m1()
+    {
+        System.out.println("tigeeeer");
+        return new B();
+    }
 }
 class Test{
-    public static void main(String[] args){
-        A a1 = new A();
-        a1.m1();
-        B b1 = new B();
-        b1.m1();
-        A a2 = new B();
-        a2.m1();
+    public static void main(String args[])
+    {
+        A a = new A();
+        a.m1();
+         B b = new B();
+         b.m1();
+         
     }
 }
