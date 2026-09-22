c97646cd6e97b1d7d2b608005b1c51c08dd171ed
ABINASH <abinashmohanty877@gmail.com>
2026-09-22 15:56:31 +0530

    Test.java

--- Thor-java/Test.java
+++ Thor-java/Test.java
@@ -1,26 +1,12 @@
 interface A{
-    public void m1(int x);
-
-}
-interface B{
-    public void m1(int y);
-}
-class Test implements A , B{
-    public void m1(int x){
-        System.out.println("abinash mohanty");
+    public static void m1()
+    {
+        System.out.println("abhiiiiiii");
     }
-
-public static void main(String[] args0){
-    Test t = new Test();
-    t.m1(1000);
-    
-    A a = t;
-    a.m1(5565);
-
-    B b = t;
-    b.m1(5555);
-
-
 }
-
+class Test implements A
+{
+    public static void main (String[] args){
+        A.m1();
+    }
 }
