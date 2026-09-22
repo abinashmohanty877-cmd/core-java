0c44317747874c48c7cead2514ec68d58ea8883a
ABINASH <abinashmohanty877@gmail.com>
2026-09-22 23:32:46 +0530

    Test.java

--- Thor-java/Test.java
+++ Thor-java/Test.java
@@ -1,18 +1,24 @@
 interface A{
     default void m1()
     {
-    System.out.println("johnnn");
+        System.out.println("abhiiiiii");
     }
 }
-class Test implements A {
-    
-        public void m1()
-        {
-            System.out.println("motuuuu");
-        }
-        public  static void main(String[] args){
-            Test t1 = new Test();
-            t1.m1();
-        }
+class Test implements A
+{
+   public void m1() 
+   {
+    System.out.println("tigerrrrr");
+   }
+
+public static void main(String[] args){
+ 
+    Test t1 = new Test();
+    t1.m1();
+
     
 }
+}
+
+
+
