71a670703244d3b284f9659ab040e3cb8ff72309
ABINASH <abinashmohanty877@gmail.com>
2026-09-23 22:58:47 +0530

    Test.java

--- Thor-java/Test.java
+++ Thor-java/Test.java
@@ -1,16 +1,24 @@
 
-class Demo
-{
-    static
-    {
-        System.out.println("captain");
-    }
-}
 class Test{
-    public static void main(String[] args)
-    {
-        System.out.println("thor");
+     static int x = 10;
+     static{
+        System.out.println("Hulkkkkk");
+        m1();
+     }
+     public static void m1(){
+        System.out.println ("hourseee");
+     }
+     static{
+        System.out.println("iron man");
+        m2();
+     }
+     public static void m2(){
+        System.out.println("tigerrrrrr");
     }
-    
-
+     static int y = 20;
+     public static void main(String[] args){
+        System.out.println("houseeee");
+        Test.m1();
+        System.out.println("mouseeee");
+    }
 }
