0dd862beda31eac677e22419fc7df0f66912d8dc
ABINASH <abinashmohanty877@gmail.com>
2026-09-24 15:00:09 +0530

    Demo.java

--- Demo.java
+++ Demo.java
@@ -1,3 +1,17 @@
-public class Demo {
-    
+class Demo
+{
+    void Demo()
+    {
+        System.out.println("abc");
+    }
+    Demo()
+    {
+        System.out.println("deff");
+    }
+    public static void main(String[] args)
+    {
+        Demo d1 = new Demo();
+        d1.Demo();
+    }
+
 }
