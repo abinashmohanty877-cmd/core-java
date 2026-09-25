a8d994a1503976cdf80d40b732af212d8049adb1
ABINASH <abinashmohanty877@gmail.com>
2026-09-25 14:42:43 +0530

    Demo.java

--- Main.java
+++ Main.java
@@ -1,22 +1,15 @@
-
-interface A {
-    void show();
-}
-
-interface B {
-    void show();
-}
-
-class C implements A, B {
-    
-    public void show() {
-        System.out.println("what a man broooo");
+class Test{
+    void show(int a){
+        System.out.println("integer method");
+    }
+    void show(String s){
+        System.out.println("string methoid");
     }
 }
-
-public class Main {
-    public static void main(String[] args) {
-        C obj = new C();
-        obj.show();
+public class Main{
+    public static void main(String[] args){
+        Test t1 = new Test();
+        t1.show(100);
+        t1.show("abhiiiii");
     }
 }
