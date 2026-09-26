30053ac9be96c4c75cddcc5ca928fec0c4d894fe
ABINASH <abinashmohanty877@gmail.com>
2026-09-26 22:00:35 +0530

    Test.java

--- Thor-java/Test.java
+++ Thor-java/Test.java
@@ -1,26 +1,15 @@
-class parent
-{
-   public void m1()
-   {
-      System.out.println("tommmy");
-   }
-}
-class child extends parent
-{
-   public void m2()
-   {
-      System.out.println("child tommy");
-   }
-}
-class Test
-{
-   public static void main(String[] args){
-      parent p1 = new parent();
-      p1.m1();
-      System.out.println("----------");
-      child c1 = new child();
-      c1.m2();
-      c1.m2();
-   }
-
+class Test{
+    Test()
+    {
+        System.out.println("constructor");
+    }
+    {
+        System.out.println("instan block");
+    }
+    public static void main(String[] args)
+    {
+        System.out.println("main start");
+        new Test();
+        System.out.println("main end");
+    }
 }
